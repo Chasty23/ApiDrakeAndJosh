312914037e1327f316c402ca8d82437e9fbcf38a
Chasty23 <stivencubiasc84@gmail.com>
2026-01-08 17:44:49 -0600

    Chance carpet

--- obj/Debug/net9.0/api.AssemblyInfo.cs
+++ obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("esta es una api de prueba desarrollada con asp.net ")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a236093611baec4ab77cd0fa148da26cf3e0f74b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+992e692142ba532f395d13cfd225926cdbe1b66f")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
