25eabe5f4c8cf6a692ea3c3b53e5251279725605
Chasty23 <stivencubiasc84@gmail.com>
2026-01-08 17:15:19 -0600

    style: Change metadata

--- obj/Debug/net9.0/api.AssemblyInfo.cs
+++ obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("esta es una api de prueba desarrollada con asp.net ")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abcc4cafdfeb906688075cf7fd3934fc23f2ea87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a236093611baec4ab77cd0fa148da26cf3e0f74b")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
