477bb2d021c6d44a2038e51c9ed0533893c2a9f2
Chasty23 <stivencubiasc84@gmail.com>
2026-01-08 18:24:01 -0600

    chore: agregate package EFC and postgresql

--- obj/Debug/net9.0/api.AssemblyInfo.cs
+++ obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("esta es una api de prueba desarrollada con asp.net ")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+312914037e1327f316c402ca8d82437e9fbcf38a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a1e38a544b8e3a6d0e7f5adec1ea9a0d7fe7a72")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
