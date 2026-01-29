354eed228775fbb534b4b3b865610886f9cd6e1c
Chasty23 <stivencubiasc84@gmail.com>
2026-01-28 18:51:05 -0600

    feat: add phrase management and enhance character creation with phrase and gender validation.

--- Mappers/CharacterMapper.cs
+++ Mappers/CharacterMapper.cs
@@ -13,5 +13,17 @@ public partial class CharacterMapper
 
     [MapperIgnoreTarget(nameof(Character.Gender))]
     [MapperIgnoreTarget(nameof(Character.Phrases))]
-    public partial Character ToCreatedDto(CharacterCreatedDto characterCreatedDto);
+    public partial Character ToCharacterDto(CharacterDto characterDto);
+
+    [MapperIgnoreTarget(nameof(Character.Id))]
+    [MapperIgnoreTarget(nameof(Character.Gender))]
+    [MapperIgnoreTarget(nameof(Character.Phrases))]
+    [MapperIgnoreSource(nameof(CharacterCreatedDto.IdPhrases))]
+    public partial Character ToCreatedEntity(CharacterCreatedDto characterCreatedDto);
+
+    [MapperIgnoreSource(nameof(Character.Id))]
+    [MapperIgnoreSource(nameof(Character.Gender))]
+    [MapperIgnoreSource(nameof(Character.Phrases))]
+    [MapperIgnoreTarget(nameof(CharacterCreatedDto.IdPhrases))]
+    public partial CharacterCreatedDto ToCharacterCreatedDto(Character character);
 }
