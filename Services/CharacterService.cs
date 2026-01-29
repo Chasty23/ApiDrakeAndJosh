ab6264c7e2fd4e8017fa5d381519a537d8f85771
Chasty23 <stivencubiasc84@gmail.com>
2026-01-28 18:56:16 -0600

    feat: Implement AppContextDb, Logger and mapper and the method post added validatios of Genders and Phrases

--- Services/CharacterService.cs
+++ Services/CharacterService.cs
@@ -2,67 +2,69 @@ using api.Models;
 using api.Dtos;
 using FluentResults;
 using api.Mappers;
+using api.DbContextApp;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace api.Services;
 
 public class CharacterService : ICharacterService
 {
-    private readonly List<Character> _characters = [];
     private readonly CharacterMapper _characterMapper;
+    private readonly ILogger<CharacterService> _logger;
+    private readonly AppContextDb _context;
 
-    public CharacterService(CharacterMapper characterMapper)
+    public CharacterService(CharacterMapper characterMapper, ILogger<CharacterService> logger, AppContextDb context)
     {
         _characterMapper = characterMapper;
-        _characters.Add(new Character
-        {
-            Id = 1,
-            Name = "Drake",
-            Surname = "Parker",
-            NameRealComplete = "Drake Parker",
-            IdGender = 1,
-            PathImage = "",
-            DateBirthDay = new BirthDay(21, 12)
-        });
-        _characters.Add(new Character
-        {
-            Id = 2,
-            Name = "Josh",
-            Surname = "Nicols",
-            NameRealComplete = "Josh Nicols",
-            IdGender = 1,
-            PathImage = "",
-            DateBirthDay = new BirthDay(22, 10)
-        });
+        _logger = logger;
+        _context = context;
+
     }
 
     public Task<List<CharacterDto>> GetAll()
     {
-        return Task.FromResult(_characters.Select(_characterMapper.ToDto).ToList());
+        return Task.FromResult(_context.Characters.Select(_characterMapper.ToDto).ToList());
     }
 
     public Task<Result<CharacterDto>> GetById(int id)
     {
-        var character = _characters.FirstOrDefault(c => c.Id == id);
+        var character = _context.Characters.FirstOrDefault(c => c.Id == id);
 
         return Task.FromResult(character == null ?
         Result.Fail("Character not found")
         : Result.Ok(_characterMapper.ToDto(character)));
     }
 
-    public Task<Result<CharacterCreatedDto>> Add(CharacterCreatedDto character)
+    public async Task<Result<CharacterCreatedDto>> Add(CharacterCreatedDto characterDto)
     {
-        var newCharacter = _characterMapper.ToCreatedDto(character);
+
+        var newCharacter = _characterMapper.ToCreatedEntity(characterDto);
+
         if (newCharacter == null)
         {
-            return Task.FromResult(Result.Fail<CharacterCreatedDto>("Character Not Created"));
+            return Result.Fail<CharacterCreatedDto>("Character Not Created");
         }
-        _characters.Add(newCharacter);
-        return Task.FromResult(Result.Ok(character));
+        var genderValid = await _context.Genders.FirstOrDefaultAsync(g => g.Id == characterDto.IdGender);
+        if (genderValid == null)
+        {
+            return Result.Fail<CharacterCreatedDto>("Gender not found");
+        }
+        var phrasesValid = await _context.Phrases.Where(p => characterDto.IdPhrases.Contains(p.Id)).ToListAsync();
+        if (phrasesValid.Count != characterDto.IdPhrases.Count)
+        {
+            return Result.Fail<CharacterCreatedDto>("Phrases not found");
+        }
+
+        _context.Characters.Add(newCharacter);
+        _context.SaveChanges();
+        _logger.LogDebug("Character added successfully");
+        return Result.Ok(_characterMapper.ToCharacterCreatedDto(newCharacter));
     }
 
     public Task<Character> Update(int id, Character character)
     {
-        var characterToUpdate = _characters.FirstOrDefault(c => c.Id == id);
+        var characterToUpdate = _context.Characters.FirstOrDefault(c => c.Id == id);
         if (characterToUpdate == null)
         {
             return Task.FromResult(character);
@@ -73,6 +75,8 @@ public class CharacterService : ICharacterService
         characterToUpdate.Phrases = character.Phrases;
         characterToUpdate.PathImage = character.PathImage;
         characterToUpdate.DateBirthDay = character.DateBirthDay;
+        _context.SaveChanges();
+        _logger.LogDebug("Character updated successfully");
         return Task.FromResult(characterToUpdate);
     }
 
