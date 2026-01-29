858c57cebeb2d9ce1699c830b3661a1ea53e7cad
Chasty23 <stivencubiasc84@gmail.com>
2026-01-28 18:48:40 -0600

    feat: Initialize core API infrastructure including data context, services, DTOs, mappers, validators, and controllers for character, gender, and phrase management.

--- Controllers/GenderController.cs
+++ Controllers/GenderController.cs
@@ -1,16 +1,42 @@
 using Microsoft.AspNetCore.Mvc;
+using api.Dtos;
+using api.Services;
+using api.Utils;
+using api.Models;
 
 namespace api.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class GenderController : ControllerBase
+public class GenderController(IGenderService genderService) : ControllerResponse
 {
 
+    private readonly IGenderService _genderService = genderService;
+
     [HttpGet]
-    public async Task<ActionResult> GetAll()
+    public async Task<ActionResult<List<GenderDto>>> GetAll()
     {
-        return Ok("Hello genders");
+        var genders = await _genderService.GetAll();
+
+        return SuccessResponse(genders, "Genders", StatusCodes.Status200OK);
+    }
+    [HttpPost]
+    public async Task<ActionResult<GenderDto>> Create([FromBody] GenderDto genderDto)
+    {
+        var gender = new Gender
+        {
+            Name = genderDto.Name
+        };
+        var genderCreate = await _genderService.Create(gender);
+        if (genderCreate.IsFailed)
+        {
+            var errorMessage = genderCreate.Errors[0].Message;
+            return ErrorResponse(errorMessage, StatusCodes.Status400BadRequest);
+        }
+
+        return SuccessResponse(genderCreate.Value, "Gender Added", StatusCodes.Status201Created);
+
+
     }
 
 }
