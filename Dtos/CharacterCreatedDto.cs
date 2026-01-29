8b2a53a3ac1199610f33e2f2d603d855a3d3c410
Chasty23 <stivencubiasc84@gmail.com>
2026-01-28 18:49:42 -0600

    feat: Add DTOs, services, mappers, and validators for Gender, Character, and Phrase entities.

--- Dtos/CharacterCreatedDto.cs
+++ Dtos/CharacterCreatedDto.cs
@@ -4,14 +4,13 @@ namespace api.Dtos;
 
 public class CharacterCreatedDto
 {
-    public int Id { get; set; }
     public required string Name { get; set; }
-    public required string Surname { get; set; }
+    public string? Surname { get; set; }
     public required string NameRealComplete { get; set; }
     public required int IdGender { get; set; }
-    public required int IdPhrases { get; set; }
+    public ICollection<int> IdPhrases { get; set; } = [];
     public string PathImage { get; set; } = "Unknown";
-    public BirthDay? DateBirthDay { get; set; }
+    public BirthDay DateBirthDay { get; set; } = new BirthDay(0, 0);
 }
 
 
